15c697f2bb0453e7db038168092c777bdccf487f
Gentility01 <mastergentility5gmail.com>
2026-09-09 14:09:01 +0100

    Hotfixes

--- core/applications/users/urls.py
+++ core/applications/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views import superuser_signup
 from .views import user_detail_view
 from .views import user_redirect_view
 from .views import user_update_view
@@ -9,4 +10,6 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
+    path("superuser/",view=superuser_signup, name="superuser_signup"),
+
 ]
